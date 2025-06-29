3078b96e7b944ac6b5235365b578b1c6ddc080bb
IXaturn <gmingquan96@gmail.com>
2025-06-29 16:01:33 +0800

    Made small server changes in app.py

--- app.py
+++ app.py
@@ -39,11 +39,12 @@ csrf = CSRFProtect(app)
 mail       = Mail(app)
 serializer = URLSafeTimedSerializer(app.secret_key)
 
-MAIL_SERVER            = os.getenv('MAIL_SERVER'),
-MAIL_PORT              = int(os.getenv('MAIL_PORT', 587)),
-MAIL_USE_TLS           = os.getenv('MAIL_USE_TLS', 'True') == 'True',
-MAIL_USERNAME          = os.getenv('MAIL_USERNAME'),
-MAIL_PASSWORD          = os.getenv('MAIL_PASSWORD'),
+MAIL_SERVER            = os.getenv('MAIL_SERVER')
+MAIL_PORT              = int(os.getenv('MAIL_PORT', 587))
+MAIL_USE_TLS           = os.getenv('MAIL_USE_TLS', 'True') == 'True'
+MAIL_USE_SSL           = os.getenv('MAIL_USE_SSL', 'False') == 'True'
+MAIL_USERNAME          = os.getenv('MAIL_USERNAME')
+MAIL_PASSWORD          = os.getenv('MAIL_PASSWORD')
 MAIL_DEFAULT_SENDER    = (os.getenv('MAIL_DEFAULT_SENDER_NAME'),os.getenv('MAIL_DEFAULT_SENDER_EMAIL'))
 
 # cf key.
