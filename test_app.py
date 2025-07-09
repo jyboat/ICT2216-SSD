8fb0b618bcc060c9cd626654f30c6396391f353b
IXaturn <gmingquan96@gmail.com>
2025-07-09 18:48:57 +0800

    Removed WTF_CSRF_ENABLED

--- test_app.py
+++ test_app.py
@@ -4,7 +4,6 @@ from app import app
 @pytest.fixture
 def client():
     app.config['TESTING'] = True
-    app.config['WTF_CSRF_ENABLED'] = False  # Disable CSRF for tests
     with app.test_client() as client:
         yield client
 
